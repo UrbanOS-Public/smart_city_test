34917a2844f7265c0c11bb7928418cabfcd15856
Richard Jones <rajones@hntb.com>
2019-05-14 16:37:51 -0400

    SMRT-1238 Building dockerfile
    co-authored-by: Nathaniel Stevens <nstevens@pillartechnology.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 echo "Running deploy script...."
 # Relies on HEX_API_KEY environment varibale.
+export HEX_API_KEY=$HEX_API_PUB_KEY
+mix hex.organization auth smartcolumbus_os --key $HEX_API_KEY 
+
 mix hex.publish --yes
