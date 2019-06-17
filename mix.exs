83bab40eaef42b4612313cd550e960616f2f90ed
Benjamin Brewer <benjamin.brewer@accenture.com>
2019-06-17 10:15:09 -0400

    bump version for release
    co-authored-by: jeffgrunewald <jeff@grunewalddesign.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCityTest.MixProject do
   def project do
     [
       app: :smart_city_test,
-      version: "0.2.7",
+      version: "0.3.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
