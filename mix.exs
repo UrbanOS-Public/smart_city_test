525462204782ae7ad3a0a4eb2028edffd248d408
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-03-02 16:34:57 -0600

    Up version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCityTest.MixProject do
   def project do
     [
       app: :smart_city_test,
-      version: "2.1.0",
+      version: "2.1.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
