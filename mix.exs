3c571bfa13c4ad4a820325a23d2ff2f86520e094
Christine Walsh <christine.walsh@accenture.com>
2022-06-27 14:47:03 -0400

    increment mix.exs
    Datastillery/internal#749

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCityTest.MixProject do
   def project do
     [
       app: :smart_city_test,
-      version: "2.2.2",
+      version: "2.2.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
